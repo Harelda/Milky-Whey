345d42b4bd8b4f90aa49ba6bdb97ad7a53460a09
Sing Wong <eiffelynx@gmail.com>
2018-01-27 10:40:44 -0800

    ran_res_color_update

--- Milky Whey/Assets/random_resourse.cs
+++ Milky Whey/Assets/random_resourse.cs
@@ -4,27 +4,36 @@ using UnityEngine;
 
 public class random_resourse : MonoBehaviour {
     public int health = 1;
-    public int energy_prove = 0;
-    public int money_prove = 0;
-    public int other_prove = 0;
+    public int min_prove = 0;
+    public int gas_prove = 0;
+    public int power_prove = 0;
     public bool in_range = false;
     public bool line_of_sight = false;
+    public int res_type = -1;
 
 
     // Use this for initialization
     void Start () {
         int res_type = Random.Range(0, 3);
         if (res_type == 0){
-            energy_prove = 25;
+            min_prove = 25;
+            GetComponent<SpriteRenderer>().color = new Color(1, 0, 0, 1);
+            ResourceManager.instance.AddCertainResource((ResourceType)res_type, 25);
+
         }
         else if(res_type == 1)
         {
-            money_prove = 25;
+            gas_prove = 25;
+            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+            ResourceManager.instance.AddCertainResource((ResourceType)res_type, 25);
         }
         else if(res_type == 2)
         {
-            other_prove = 25;
+            power_prove = 25;
+            GetComponent<SpriteRenderer>().color = new Color(1, 0.92f, 0.016f, 1);
+            ResourceManager.instance.AddCertainResource((ResourceType)res_type, 25);
         }
+
     }
     
 
