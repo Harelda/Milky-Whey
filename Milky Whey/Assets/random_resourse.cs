91d15b747921f954085a16e3112d92974c1e1959
Sing Wong <eiffelynx@gmail.com>
2018-01-27 09:33:42 -0800

    random_res_update

--- Milky Whey/Assets/random_resourse.cs
+++ Milky Whey/Assets/random_resourse.cs
@@ -1,28 +1,37 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class random_resourse : MonoBehaviour {
-    private int health = 1;
-    private int energy_prove = 0;
-    private int money_prove = 0;
-    private int other_prove = 0;
-    private bool in_range = false;
-    
+    public int health = 1;
+    public int energy_prove = 0;
+    public int money_prove = 0;
+    public int other_prove = 0;
+    public bool in_range = false;
+    public bool line_of_sight = false;
 
 
     // Use this for initialization
     void Start () {
-        int rng = Rand.Range(0, 3);
-
-        print(rng);
+        int res_type = Random.Range(0, 3);
+        if (res_type == 0){
+            energy_prove = 25;
+        }
+        else if(res_type == 1)
+        {
+            money_prove = 25;
+        }
+        else if(res_type == 2)
+        {
+            other_prove = 25;
+        }
     }
     
 
     // Update is called once per frame
     void Update () {
+
         
         
-}
+    }
 }
