6626d14df15c3298d060c96d21a7766f01dcb12e
Richard But <butr@uci.edu>
2018-01-27 18:27:58 -0800

    Level 1 Resource UI
    Added Resource UI to Level 1

--- Milky Whey/Assets/Scripts/UI/ResourceText.cs
+++ Milky Whey/Assets/Scripts/UI/ResourceText.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ResourceText : MonoBehaviour {
+    private Text[] resourceTexts;
 
 	// Use this for initialization
 	void Start () {
-		
+        resourceTexts = transform.GetComponentsInChildren<Text>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        for (int i = 0; i < resourceTexts.Length; i++)
+        {
+            resourceTexts[i].text = ResourceManager.instance.resources[i].ToString();
+        }
+    }
 }
