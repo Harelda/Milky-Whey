6fc7f78b72ef486ac2db97f5c49eba541777900a
Chieh-Fu Chen <chiehfc@uci.edu>
2018-01-28 11:53:21 -0800

    Added stage select menu.

---
+++ Milky Whey/Assets/StageSelectMenuScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StageSelectMenuScript : MonoBehaviour {
+
+	public Button[] stageButtons;
+	public Text[] stageTexts;
+	public Image[] unknownPlanets;
+
+	public Image[] lines;
+
+	private int level=1;
+
+	// Use this for initialization
+	void Start () {
+		PlayerPrefs.SetInt ("Level", 4);
+		level = PlayerPrefs.GetInt("Level");
+
+		for (int i = 0; i < level; i++) {
+			stageButtons [i].gameObject.SetActive (true);
+			stageTexts [i].gameObject.SetActive (true);
+			unknownPlanets [i].gameObject.SetActive (false);
+			if (i != 0) {
+				lines [i - 1].gameObject.SetActive (true);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void LoadLevel(int levelIndex)
+	{
+		SceneManager.LoadScene (levelIndex);
+	}
+}
